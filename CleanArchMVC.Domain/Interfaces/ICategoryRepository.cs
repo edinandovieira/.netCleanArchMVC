7d565ea94dbd70829f6ec92865fea09e43970cc1
Edinando Vieira <edvieira93@gmail.com>
2024-02-09 15:47:02 -0300

    ✨ feat: Add repository interfaces

---
+++ CleanArchMVC.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchMVC.Domain.Entities;
+
+namespace CleanArchMVC.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetByIdAsync(int? id);
+        Task<Product> CreateAsync(Product product);
+        Task<Product> UpdateAsync(Product product);
+        Task<Product> RemoveAsync(Product product);
+    }
+}
---
+++ CleanArchMVC.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchMVC.Domain.Entities;
+
+namespace CleanArchMVC.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Category>> GetCategories();
+        Task<Category> GetById(int? id);
+        Task<Category> Create(Category category);
+        Task<Category> Update(Category category);
+        Task<Category> Remove(Category category);
+    }
+}
