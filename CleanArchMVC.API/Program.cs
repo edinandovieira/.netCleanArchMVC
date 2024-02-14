2dc5177f62177a610d88ead7f806689095d6801d
Edinando Vieira <edvieira93@gmail.com>
2024-02-14 10:29:26 -0300

    ✨ feat: Add Web API Project with Categories Controller

---
+++ CleanArchMVC.API/Program.cs
@@ -0,0 +1,39 @@
+using CleanArchMVC.Domain.Account;
+using CleanArchMVC.Infra.IoC;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddInfraestructureAPI(builder.Configuration);
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+using (var serviceScope = app.Services.CreateScope())
+{
+    var services = serviceScope.ServiceProvider;
+
+    var seedUserRoleInitial = services.GetRequiredService<ISeedUserRoleInitial>();
+
+    seedUserRoleInitial.SeedRoles();
+    seedUserRoleInitial.SeedUsers();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
